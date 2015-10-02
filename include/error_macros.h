3e7a3e74a1172d1516c7a9e24ec675248feaefae
Michal Cyprian <m.cyprian@gmail.com>
2015-10-02 22:00:51 +0200

    Add error_macros header - 2 levels of error macros, - macro for each of interpret return codes - warning macro - assertation of args values

---
+++ include/error_macros.h
@@ -0,0 +1,231 @@
+/**
+ * @file error_macros.h
+ * @author Michal Cyprian <xcypri01.stud.fit.vutbr.cz>
+ *
+ * @section DESCRIPTION
+ *
+ * Collection of error and warning macros to check functions return codes
+ * and handle specific situations
+ */
+
+#ifndef ERROR_MACROS_H
+#define ERROR_MACROS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/** Prints warning message to stderr
+ * @param var vatiable to be checked
+ * @param value value indicating unexpected situation
+ * @param msg warning message
+ */
+#define catch_warning(var, value, msg)                               \
+    do {                                                             \
+        if ((var) == (value)) {                                      \
+                fprintf(stderr, "[WARNING] (%s:%s:%d) " msg "\n",    \
+                        __FILE__, __func__, __LINE__);               \
+        }                                                            \
+    } while (0)
+
+/** Lexical analysis error, wrong structure of lexem.
+ * @param var variable to be checked
+ * @param value value indicating error
+ * @param msg error message
+ * @param v set macro to be verbal
+ * @return one if error occures
+ */
+#define catch_lexical_error(var, value, msg, v)                        \
+    do {                                                               \
+        if ((var) == (value)) {                                        \
+            if(v)                                                      \
+                fprintf(stderr, "[LEXICAL ERROR] (%s:%s:%d) " msg "\n",\
+                        __FILE__, __func__, __LINE__);                 \
+                return 1;                                              \
+        }                                                              \
+    } while (0)
+
+/** Syntax analysis error.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ * @return two if error occures
+ */
+#define catch_syntax_error(var, value, msg, v)                         \
+    do {                                                               \
+        if ((var) == (value)) {                                        \
+            if(v)                                                      \
+                fprintf(stderr, "[SYNTAX ERROR] (%s:%s:%d) " msg "\n", \
+                        __FILE__, __func__, __LINE__);                 \
+                return 2;                                              \
+        }                                                              \
+    } while (0)
+
+/** Undefined function/variable error.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ * @return three if error occures
+ */
+#define catch_undefined_error(var, value, msg, v)                        \
+    do {                                                                 \
+        if ((var) == (value)) {                                          \
+            if(v)                                                        \
+                fprintf(stderr, "[SEMANTIC ERROR] (%s:%s:%d) " msg "\n", \
+                        __FILE__, __func__, __LINE__);                   \
+                return 3;                                                \
+        }                                                                \
+    } while (0)
+
+
+/** Type compatibility error.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ * @return four if error occures
+ */
+#define catch_compatibility_error(var, value, msg, v)                    \
+    do {                                                                 \
+        if ((var) == (value)) {                                          \
+            if(v)                                                        \
+                fprintf(stderr, "[SEMANTIC ERROR] (%s:%s:%d) " msg "\n", \
+                        __FILE__, __func__, __LINE__);                   \
+                return 4;                                                \
+        }                                                                \
+    } while (0)
+
+/** Data type selection error.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ * @return five if error occures
+ */
+#define catch_type_error(var, value, msg, v)                             \
+    do {                                                                 \
+        if ((var) == (value)) {                                          \
+            if(v)                                                        \
+                fprintf(stderr, "[TYPE ERROR] (%s:%s:%d) " msg "\n",     \
+                        __FILE__, __func__, __LINE__);                   \
+                return 5;                                                \
+        }                                                                \
+    } while (0)
+
+/** Other semantic errors.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ * @return six if error occures
+ */
+#define catch_semantic_error(var, value, msg, v)                         \
+    do {                                                                 \
+        if ((var) == (value)) {                                          \
+            if(v)                                                        \
+                fprintf(stderr, "[SEMANTIC ERROR] (%s:%s:%d) " msg "\n", \
+                        __FILE__, __func__, __LINE__);                   \
+                return 6;                                                \
+        }                                                                \
+    } while (0)
+
+/** Input reading errors.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ *
+ * TODO free memory before exit
+ */
+#define catch_input_error(var, value, msg)                              \
+    do {                                                                \
+        if ((var) == (value)) {                                         \
+                fprintf(stderr, "[RUNTIME ERROR] (%s:%s:%d) " msg "\n", \
+                        __FILE__, __func__, __LINE__);                  \
+                exit (7);                                               \
+        }                                                               \
+    } while (0)
+
+/** Working with uninitialized variable.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ *
+ * TODO free memory before exit
+ */
+#define catch_uninicialized_error(var, value, msg)                      \
+    do {                                                                \
+        if ((var) == (value)) {                                         \
+                fprintf(stderr, "[UNINIT ERROR] (%s:%s:%d) " msg "\n",  \
+                        __FILE__, __func__, __LINE__);                  \
+                exit(8);                                                \
+        }                                                               \
+    } while (0)
+
+/** Devision by zero.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ *
+ * TODO free memory before exit
+ */
+#define catch_div_zero_error(var, value, msg)                           \
+    do {                                                                \
+        if ((var) == (value)) {                                         \
+                fprintf(stderr, "[DIV ERROR] (%s:%s:%d) " msg "\n",     \
+                        __FILE__, __func__, __LINE__);                  \
+                exit(9);                                                \
+        }                                                               \
+    } while (0)
+
+/** Other runtime errors.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ * @return ten if error occures
+ */
+#define catch_runtime_error(var, value, msg, v)                         \
+    do {                                                                \
+        if ((var) == (value)) {                                         \
+            if(v)                                                       \
+                fprintf(stderr, "[RUNTIME ERROR] (%s:%s:%d) " msg "\n"  \
+                        __FILE__, __func__, __LINE__);                  \
+                return 10;                                              \
+        }                                                               \
+    } while (0)
+
+/** Internal error in interpret.
+ * @param var variable to be checked
+ * @param value value indicating  error
+ * @param msg error message
+ * @param v set macro to be verbal
+ *
+ * TODO free memory before exit
+ */
+#define catch_internal_error(var, value, msg)                           \
+    do {                                                                \
+        if ((var) == (value)) {                                         \
+            fprintf(stderr, "[INTERNAL ERROR] (%s:%s:%d) " msg "\n",    \
+                    __FILE__, __func__, __LINE__);                      \
+            exit(99);                                                   \
+        }                                                               \
+    } while (0)
+
+/** Checks if condition is met.
+ * @param condition condition to be checked
+ * @return ret_val on assertion failed
+ */
+#define args_assert(condition, ret_val)                                        \
+    do {                                                                       \
+        if (!(condition)) {                                                    \
+            fprintf(stderr, "[ASSERTION FAILED] (%s:%s:%d) Unexpected values"  \
+                    " of function arguments.\n",__FILE__, __func__, __LINE__); \
+             return ret_val;                                                   \
+        }                                                                      \
+    } while(0)
+            
+#endif // !ERROR_MACROS_H
