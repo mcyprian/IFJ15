06ed2cfcade31af542ea1921613b3ea6254be858
MichalDurista <miskodurista@gmail.com>
2015-10-15 21:18:53 +0200

    Update scanner.c and scanner.h

--- include/scanner.h
+++ include/scanner.h
@@ -13,6 +13,42 @@
 #include <stdbool.h>
 #include <dynamic_buffer.h>
 
-enum {START, IDENTIFIER, INTEGER, DOUBLE, EQUALS, LESS, GREATER, EXCLAMATION, ARROW};
+enum
+{
+	ARROW,
+	AUTO,
+	CIN,
+	CLOSING_BRACKET,
+	CLOSING_CURLY_BRACKET,
+	CLOSING_SQUARE_BRACKET,
+	COLON,
+	COMMA,
+	COUT,
+	DOUBLE,
+	ELSE,
+	EQUALS,
+	EXCLAMATION,
+	FOR,
+	GREATER,
+	IDENTIFIER,
+	IF,
+	INT,
+	INTEGER,
+	LESS,
+	OPENING_BRACKET,
+	OPENING_CURLY_BRACKET,
+	OPENING_SQUARE_BRACKET,
+	OPERATOR,
+	RETURN,
+	SEMICOLON,
+	STRING,
+	START,
+};
+
+typedef struct
+{
+	int type;
+	char *value;
+} Token;
 
 int tokenize(FILE *fin, TDynamic_buffer *b);
--- src/scanner.c
+++ src/scanner.c
@@ -11,13 +11,88 @@
 
 #include <scanner.h>
 
+int reservedWord(char *identifier)
+{
+	switch(identifier[0])
+	{
+		case 'a':
+			if (strcmp(identifier, "auto") == 0)
+			{	
+				return AUTO;
+			}
 
-int tokenize(FILE *fin, TDynamic_buffer *b) 
+		case 'c':
+			if (strcmp(identifier, "cin") == 0)
+			{	
+				return CIN;
+			}
+			else if (strcmp(identifier, "cout") == 0)
+			{
+				return COUT;
+			}
+				
+		case 'd':
+			if (strcmp(identifier, "double") == 0)
+			{	
+				return DOUBLE;
+			}
+
+		case 'e':
+			if (strcmp(identifier, "else") == 0)
+			{	
+				return ELSE;
+			}
+
+		case 'f':
+			if (strcmp(identifier, "for") == 0)
+			{	
+				return FOR;
+			}
+
+		case 'i':
+			if (strcmp(identifier, "if") == 0)
+			{	
+				return IF;
+			}
+			else if (strcmp(identifier, "int") == 0)
+			{	
+				return INT;
+			}
+				
+		case 'r':
+			if (strcmp(identifier, "return") == 0)
+			{	
+				return RETURN;
+			}
+
+		case 's':
+			if (strcmp(identifier, "string") == 0)
+			{	
+				return STRING;
+			}
+	} // switch
+
+	return IDENTIFIER;
+} // reservedWord
+
+int tokenize(FILE *fin, TDynamic_buffer *buffer) 
 {
 	int c;
 	int previous = 0;
 	int state = START;
 	bool read = true;
+
+	/* !!!!!!!!!!!! TOTO JE LEN DOCASNE RIESENIE !!!!!!!!!!!!!!!! */
+	Token *token = (Token *) malloc(sizeof(Token));
+	if (token == NULL)
+    {
+        printf("Je to v pici, nenaalokovali sme\n");
+        fclose(fin);
+        return -1;
+    }
+
+	token->type = IDENTIFIER;	// aby tam nieco bolo
+	/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 
 	while (1) {
 		if (read)
@@ -38,87 +113,113 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 				if ((c > 64 && c < 91) || (c > 96 && c < 123)) //A-Z || a-z
 				{
 					state = IDENTIFIER;
-					add_char(b, c);
+					add_char(buffer, c);
 					
 				}
 				else if (c > 47 && c < 58)
 				{
 					state = INTEGER;
-					add_char(b, c);
+					add_char(buffer, c);
 				}
 				else  
 				{
 					switch (c)
 					{
 						case '+':
-							add_char(b, c);
-							printf("%s OPERATOR\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = OPERATOR;
+							printf("%s    %d\n", token->value, token->type);	// bude return token; velmi pravdepodobne :D // docasne vypisuje uz len cislo z enumeracie "typu" - aj tak to vypisovat uz nebude, len vvraciat token s typom
 							break;
 						case '-':
-							add_char(b, c);
-							printf("%s OPERATOR\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = OPERATOR;
+							printf("%s    %d\n", token->value, token->type);
 							
 							break;
 						case '/':
-							add_char(b, c);
-							printf("%s OPERATOR\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = OPERATOR;
+							printf("%s    %d\n", token->value, token->type);
 							
 							break;
 						case '*':
-							add_char(b, c);
-							printf("%s OPERATOR\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = OPERATOR;
+							printf("%s    %d\n", token->value, token->type);
 							
 							break;
 						case ';':
-							add_char(b, c);
-							printf("%s SEMICOLON\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = SEMICOLON;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case ':':
-							add_char(b, c);
-							printf("%s COLON\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = COLON;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case ',':
-							add_char(b, c);
-							printf("%s COMMA\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = COMMA;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case '(':
-							add_char(b, c);
-							printf("%s OPENING BRACKET\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = OPENING_BRACKET;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case ')':
-							add_char(b, c);
-							printf("%s CLOSING BRACKET\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = CLOSING_BRACKET;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case '[':
-							add_char(b, c);
-							printf("%s OPENING SQUARE BRACKET\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = OPENING_SQUARE_BRACKET;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case ']':
-							add_char(b, c);
-							printf("%s CLOSING SQUARE BRACKET\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = CLOSING_SQUARE_BRACKET;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case '{':
-							add_char(b, c);
-							printf("%s OPENING CURLY BRACKET\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = OPENING_CURLY_BRACKET;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case '}':
-							add_char(b, c);
-							printf("%s CLOSING CURLY BRACKET\n", save_token(b));
+							add_char(buffer, c);
+							token->value = save_token(buffer);
+							token->type = CLOSING_CURLY_BRACKET;
+							printf("%s    %d\n", token->value, token->type);
 							break;
 						case '=':
-							add_char(b, c);
+							add_char(buffer, c);
 							state = EQUALS;
 							break;
 						case '<':
-							add_char(b, c);
+							add_char(buffer, c);
 							state = LESS;
 							break;
 						case '>':
-							add_char(b, c);
+							add_char(buffer, c);
 							state = GREATER;
 							break;
 						case '!':
-							add_char(b, c);
+							add_char(buffer, c);
 							state = EXCLAMATION;
 							break;
 						}
@@ -128,8 +229,10 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 			case EQUALS:
 				if (c == '=')
 				{
-					add_char(b, c);
-					printf("%s OPERATOR\n", save_token(b));
+					add_char(buffer, c);
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					state = START;
 					break;
 				}
@@ -138,22 +241,28 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 					previous = c;
 					read = false;
 					state = START;
-					printf("%s OPERATOR\n", save_token(b));
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					break;
 				}
 
 			case GREATER:
 				if (c == '=')
 				{
-					add_char(b, c);
-					printf("%s OPERATOR\n", save_token(b));
+					add_char(buffer, c);
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					state = START;
 					break;
 				}
 				else if (c == '>')
 				{
-					add_char(b, c);
-					printf("%s OPERATOR\n", save_token(b));
+					add_char(buffer, c);
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					state = START;
 					break;
 				}
@@ -162,22 +271,28 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 					previous = c;
 					read = false;
 					state = START;
-					printf("%s OPERATOR\n", save_token(b));
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					break;
 				}
 
 			case LESS:
 				if (c == '=')
 				{
-					add_char(b, c);
-					printf("%s OPERATOR\n", save_token(b));
+					add_char(buffer, c);
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					state = START;
 					break;
 				}
 				else if (c == '<')
 				{
-					add_char(b, c);
-					printf("%s OPERATOR\n", save_token(b));
+					add_char(buffer, c);
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					state = START;
 					break;
 				}
@@ -186,15 +301,19 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 					previous = c;
 					read = false;
 					state = START;
-					printf("%s OPERATOR\n", save_token(b));
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					break;
 				}
 
 			case EXCLAMATION:
 				if (c == '=')
 				{
-					add_char(b, c);
-					printf("%s OPERATOR\n", save_token(b));
+					add_char(buffer, c);
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					state = START;
 					break;
 				}
@@ -203,16 +322,22 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 					previous = c;
 					read = false;
 					state = START;
-					printf("%s OPERATOR\n", save_token(b));
+					token->value = save_token(buffer);
+					token->type = OPERATOR;
+					printf("%s    %d\n", token->value, token->type);
 					break;
 				}
 
 			case IDENTIFIER:
 
 				if ((c > 64 && c < 91) || (c > 96 && c < 123) || (c > 47 && c < 58) || c == '_') { // (A-Z, a-z, 0-9, _)
-					add_char(b, c);
-				} else {
-					printf("%s IDENTIFIER\n", save_token(b));
+					add_char(buffer, c);
+				} 
+				else 
+				{
+					token->value = save_token(buffer);
+					token->type = reservedWord(token->value);
+					printf("%s    %d\n", token->value, token->type);
 					previous = c;
 					read = false;
 					state = START;
@@ -222,16 +347,18 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 			case INTEGER:
 				if (c > 47 && c < 58)
 				{
-					add_char(b, c);
+					add_char(buffer, c);
 				}
 				else if (c == 46) // c == "."
 				{
-					add_char(b, c);
+					add_char(buffer, c);
 					state = DOUBLE;
 				}
 				else
 				{
-					printf("%s INTEGER\n", save_token(b));
+					token->value = save_token(buffer);
+					token->type = INTEGER;
+					printf("%s    %d\n", token->value, token->type);
 					previous = c;
 					read = false;
 					state = START;
@@ -241,11 +368,13 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 			case DOUBLE:
 				if (c > 47 && c < 58)
 				{
-					add_char(b, c);
+					add_char(buffer, c);
 				}
 				else
 				{
-					printf("%s DOUBLE\n", save_token(b));
+					token->type = DOUBLE;
+					token->value = save_token(buffer);
+					printf("%s    %d\n", token->value, token->type);
 					previous = c;
 					read = false;
 					state = START;
@@ -255,5 +384,7 @@ int tokenize(FILE *fin, TDynamic_buffer *b)
 
 		} // switch(state)
 	} // while(1)
+
+	free(token);
 	return 0;
 }
