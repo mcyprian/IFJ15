e7b0608be262d6f00fe7090e89d3d03b0555f12a
tomassykora <sykora.tms@gmail.com>
2015-10-18 22:10:18 +0200

    Update symbol_table.h

--- include/symbol_table.h
+++ include/symbol_table.h
@@ -7,6 +7,11 @@
  * Header file for symbol_table.c module
  */
 
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include <datatypes.h>
+
  enum token_types {IDENTIFIER, OPERATOR, KEYWORD};
 
 typedef struct {
@@ -17,7 +22,8 @@ typedef struct {
 typedef struct tree {
 	Tlexeme_data lexeme;
 	int key;
-	struct tree *right, *left;
+	index_t dynamic_buffer_index;
+	struct tree *right, *left, *next;
 }Ttree;
 
 /** Initializes symbol table.
@@ -30,16 +36,23 @@ void symbol_table_init(Ttree *tree);
  * @param lexeme_identifier string identifier of searched lexeme
  * @return pointer to found object if searched token was found, NULL if was not
  */
-Ttree* search_token(Ttree **tree, char *lexeme_identifier);
+Ttree* search_symbol(Ttree **tree, char *lexeme_identifier, index_t dynamic_buffer_index);
 
 /** Adds new lexeme to symbol table
  * @param tree pointer to a symbol table
  * @param lexeme_identifier string identifier of searched lexeme
  * @return key to binary search tree, or -1 on malloc failure
  */
-int add_lexeme(Ttree **tree, char *lexeme_identifier);
+int add_symbol(Ttree **tree, char *lexeme_identifier, index_t dynamic_buffer_index);
 
 /** Delete symbol table and dealloc memory
  * @param tree pointer to a symbol table
  */
 void del_symbol_table(Ttree *tree);
+
+/** Delete linked list and dealloc memory
+ * @param tree pointer to a list node
+ */
+void del_list(Ttree *list);
+
+#endif
