f5e74cfe6854851128782984a3ed41f5a80eaf5d
Michal Cyprian <m.cyprian@gmail.com>
2015-10-24 21:37:48 +0200

    Rewrite token_types enumeration

--- include/token.h
+++ include/token.h
@@ -25,43 +25,45 @@ typedef struct {
 
 enum token_types
 {
-	ARROW, // 0
-	AUTO, // 1
-	CIN, // 2
-	CLOSING_BRACKET, // 3
-	CLOSING_CURLY_BRACKET, // 4
-	CLOSING_SQUARE_BRACKET, // 5
-	COLON, // 6
-	COMMA, // 7
-	COUT, // 8
-	DOUBLE, // 9
-	ELSE, // 10
-	EQUALS, // 11
-	EXCLAMATION, // 12
-	FOR, // 13
-	GREATER, // 14
-	IDENTIFIER, // 15
-	IF,	// 16
-	INT, // 17
-	INTEGER, // 18
-	LESS, // 19
-	MINUS, // 20
-	OPENING_BRACKET, // 21
-	OPENING_CURLY_BRACKET, // 22
-	OPENING_SQUARE_BRACKET, // 23
-	OPERATOR, // 24
-	RETURN, // 25
-	SCIENTIFIC, // 26
-	SEMICOLON, // 27
-	STRING, // 28
-	STRING_LITERAL, // 29
-	COMMENT, // 30
-	LINE_COMMENT, // 31
-	BLOCK_COMMENT, // 32
-	BLOCK_COMMENT_END, // 33
-	START, // 34
-	ERRORT, // 35
-	EOFT // 36
+	T_DOUBLE, // 0                         } <TYPE>
+	T_INT, // 1                            }
+	T_STRING, // 2                         }
+    K_CIN, // 3                               } <KEY>
+	K_COUT, // 4                              }
+    K_DO,   // 5                              }
+	K_ELSE, // 6                              }
+	K_FOR, // 7                               }
+	K_IF,	// 8                              }
+	K_RETURN, // 9                            }
+    K_WHILE,  // 10                           }
+	L_INT, // 11                                 } <RVALUE>
+    L_DOUBLE, // 12                              }
+	L_STRING, // 13                              }
+	IDENTIFIER, // 14                            }
+    O_ASSIGN, // 15                  =
+    O_DIV,   // 16                   /
+	O_EQUALS, // 17                  ==
+	O_G, // 18                       >
+    O_GE, // 19                      >=
+	O_L, // 20                       <
+    O_LE, // 21                      <=
+	O_LEFT_ARROW, // 22              <<
+    O_PLUS,  // 23                   +
+	O_RIGHT_ARROW, // 24             >>
+    O_MUL,   // 25                   *
+	O_MINUS, // 26                   -
+    O_NE,  // 27                     !=
+	UO_EXCLAMATION, // 28            !
+	AUTO, // 29                      auto
+	COMMA, // 30                     ,
+	CLOSING_BRACKET, // 31           )
+	CLOSING_CURLY_BRACKET, // 32     }
+    OPENING_BRACKET, // 33           (
+	OPENING_CURLY_BRACKET, // 34     )
+	SEMICOLON, // 35                 ;
+	START, // 36
+	ERRORT, // 37
+	EOFT // 38
 };
 
 
