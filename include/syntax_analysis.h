d64ed1f27352e111b51e34452e4e70214e3d5ad4
Radovan Sroka <rsroka@redhat.com>
2015-10-31 23:06:45 +0100

    Init syntax_analysis

---
+++ include/syntax_analysis.h
@@ -0,0 +1,15 @@
+/**
+ * @file syntax_analysis.h
+ * @author Radovan Sroka <xsroka00@stud.fit.vutbr.cz>
+ *
+ * @section DESCRIPTION
+ *
+ * Header file of module syntax_analysis.c
+ */
+
+#ifndef SYNTAX_ANALYSIS_H
+#define SYNTAX_ANALYSIS_H
+
+
+
+#endif // !SYNTAX_ANALYSIS_H
---
+++ src/syntax_analysis.c
@@ -0,0 +1,10 @@
+/**
+ * @file syntax_analysis.c
+ * @author Radovan Sroka <xsroka00@stud.fit.vutbr.cz>
+ *
+ * @section DESCRIPTION
+ *
+ * Module syntax_analysis.c
+ */
+
+#include <syntax_analysis.h>
