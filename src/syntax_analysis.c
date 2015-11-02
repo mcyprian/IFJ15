733d8e0873163aaaaaba0f10a9419fcbd4115529
Radovan Sroka <rsroka@redhat.com>
2015-11-02 01:20:37 +0100

    Add syntax analysis implementation

--- src/syntax_analysis.c
+++ src/syntax_analysis.c
@@ -18,36 +18,77 @@
 #include <datatypes.h>
 #include <resources.h>
 
-#define isUsed(x) x ? 1 : 0
-#define use(x) x = NULL;
-
 int check_syntax(NonTerminals term, Resources * resources){
 	debug_print("%s, term=%d\n", "INIT", term);
 	int iRet = RETURN_OK;
 
 	static TToken * token = NULL;
-	static index_t token_index;
+	static index_t token_index = ZERO_INDEX;
 
-	if (term == PROGRAM){
-		token_index = ZERO_INDEX;
-		token = NULL;
+	if (token == NULL){
+		token_index = get_token(resources->source, &(resources->string_buff), &(resources->struct_buff));
+		dereference_structure(&(resources->struct_buff), token_index, (void **)&token);
 	}
 
-	if (isUsed(token)){
-		token_index = get_token(resources->source, &(resources->string_buff), &(resources->struct_buff));
-		dereference_structure(&(resources->struct_buff), token_index, (void **)token);
+	if (token->token_type == ERRORT){
+		iRet = LEXICAL_ERROR;
+		goto DEFAULT;
 	}
 
 	switch(term){
+
+//**************** PROGRAM **********************//
 		case PROGRAM:
 			if ((iRet = check_syntax(FUNC, resources)) != 0)goto DEFAULT;
 			if ((iRet = check_syntax(PROGRAM_N, resources)) != 0)goto DEFAULT;
 			break;
+
+
+//**************** PROGRAM_N **********************//
 		case PROGRAM_N:
-			if ((iRet = check_syntax(FUNC, resources)) != 0)goto DEFAULT;
+			if (token->token_type == EOFT)
+				goto DEFAULT;
+			else if (token->token_type >= T_DOUBLE && token->token_type <= T_STRING){
+				if ((iRet = check_syntax(PROGRAM, resources)) != 0)goto DEFAULT;
+			}
+			else {
+				iRet = SYNTAX_ERROR;
+				goto DEFAULT;
+			}			
 			break;
+
+
+//**************** BLOCK_STATMENT **********************//
 		case BLOCK_STATMENT:
+			if ((token->token_type >= T_DOUBLE && token->token_type <= T_STRING) || (token->token_type == AUTO)){
+				if ((iRet = check_syntax(DEC_VAR, resources)) != 0)goto DEFAULT;
+				if (token->token_type != SEMICOLON){
+					iRet = SYNTAX_ERROR;
+					goto DEFAULT;
+				} else token = NULL; 
+				if ((iRet = check_syntax(BLOCK_STATMENT, resources)) != 0)goto DEFAULT;
+			}
+			else if (token->token_type == IDENTIFIER){
+				if ((iRet = check_syntax(ASSIGNMENT, resources)) != 0)goto DEFAULT;
+				if (token->token_type != SEMICOLON){
+					iRet = SYNTAX_ERROR;
+					goto DEFAULT;
+				} else token = NULL;
+				if ((iRet = check_syntax(BLOCK_STATMENT, resources)) != 0)goto DEFAULT;
+			}
+			else if ((token->token_type >= K_CIN && token->token_type <= K_DO) || 
+						(token->token_type >= K_FOR && token->token_type <= K_WHILE)){
+				if ((iRet = check_syntax(CONSTRUCTION, resources)) != 0)goto DEFAULT;
+				if ((iRet = check_syntax(BLOCK_STATMENT, resources)) != 0)goto DEFAULT;
+			}
+			else {
+				iRet = SYNTAX_ERROR;
+				goto DEFAULT;
+			}
 			break;
+
+
+//**************** FUNC **********************//
 		case FUNC:
 			break;
 		case STATIC_PART:
