b80c1576fdb0697e262d0e9cd49a518f4a6cc5b7
tomassykora <sykora.tms@gmail.com>
2015-11-12 23:51:36 +0100

    Update symbol_table.h

--- include/symbol_table.h
+++ include/symbol_table.h
@@ -11,48 +11,114 @@
 #define SYMBOL_TABLE_H
 
 #include <datatypes.h>
+#include <resources.h>
 
-// enum token_types {IDENTIFIER, OPERATOR, KEYWORD};
+#define NOT_FOUND 99
+#define EMPTY_TREE 99
 
-typedef struct {
-	int token_data_type;
-	char *lexeme_identifier;
-}Tlexeme_data;
+enum data_types{
+	INT,
+	FLOAT,
+	STRING
+};
+
+enum types_of_tokens{ 
+	VAR, 
+	FUNC 
+};
+
+union Values {
+	int i;	//for integer variable or number of arguments of function
+	float f;	//for float variable
+	index_t index_to_dynamic_buff;	//for string variable
+};
+
+typedef struct func_args {
+	int data_type;
+	index_t index_to_string_identifier;
+}TFunc_args;
 
 typedef struct tree {
-	Tlexeme_data lexeme;
 	unsigned long key;
-	index_t dynamic_buffer_index;
-	struct tree *right, *left, *next;
-}Ttree;
+	index_t index_to_dynamic_buff;
+	index_t right, left, next;
+	int type;	//function or variable
+	union Values var_value;
+	bool declared;
+	int data_type;	//(for function it means return value) one of enum data_types
+	TFunc_args *args;
+}TTree;
 
-/**
-*/
-unsigned long hash_key(char *str);
 
-/** Searches exact lexeme in symbol table according to its identifier.
- * @param tree pointer to a symbol table
- * @param lexeme_identifier string identifier of searched lexeme
- * @return pointer to found object if searched token was found, NULL if it was not
+/** Inserts new node of variable to symbol talbe tree
+ * @param resources pointer to structure with buffers
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ * @param data_type data type of new variable
+ * @return 0 on success, INTERNAL_ERROR on error
  */
-Ttree* search_symbol(Ttree **tree, char *lexeme_identifier);
+int declare_variable(Resources *resources, index_t index_to_string, index_t *index_to_root_node, int data_type);
 
-/** Adds new lexeme to symbol table
- * @param tree pointer to a symbol table
- * @param lexeme_identifier string identifier of searched lexeme
- * @return key to binary search tree, or -1 on malloc failure
+/** Sets value to float variable
+ * @param resources pointer to structure with buffers
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ * @param value value of variable to be stored in symbol table
+ * @return 0 on success, INTERNAL_ERROR on error
  */
-int add_symbol(Ttree **tree, char *lexeme_identifier, index_t dynamic_buffer_index);
+int set_float_value(Resources *resources, index_t index_to_string, index_t *index_to_root_node, float value);
 
-/** Delete symbol table and dealloc memory
- * @param tree pointer to a symbol table
+/** Sets value to integer variable
+ * @param resources pointer to structure with buffers
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ * @param value value of variable to be stored in symbol table
+ * @return 0 on success, INTERNAL_ERROR on error
  */
-void del_symbol_table(Ttree *tree);
+int set_int_value(Resources *resources, index_t index_to_string, index_t *index_to_root_node, int value);
 
-/** Delete linked list and dealloc memory
- * @param tree pointer to a list node
+/** Sets value to string variable
+ * @param resources pointer to structure with buffers
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ * @param index_to_dynamic_buff index to string stored in dynamic buffer
+ * @return 0 on success, INTERNAL_ERROR on error
  */
-void del_list(Ttree *list);
+int set_string_value(Resources *resources, index_t index_to_string, index_t *index_to_root_node, index_t index_to_dynamic_buff);
 
+/** Inserts new node of function to symbol talbe tree
+ * @param resources pointer to structure with buffers
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ * @param ret_val data type of return value of the function
+ * @param num_of_args number of arguments
+ * @return 0 on success, INTERNAL_ERROR on error
+ */
+int declare_function(Resources *resources, index_t index_to_string, index_t *index_to_root_node, int ret_val, int num_of_args);
+
+/** Adds another argument to declared function in symbol table
+ * @param resources pointer to structure with buffers
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ * @param data_type data type of the argument
+ * @param index_to_dynamic_buff index to string of argument's identifier in dynamic buffer
+ * @return 0 on success, INTERNAL_ERROR on malloc or realloc error
+ */
+int add_func_arg(Resources *resources, index_t index_to_string, index_t *index_to_root_node, int data_type, index_t index_to_dynamic_buff);
+
+/** Finds out wether the function or variable was declared before
+ * @param resources pointer to structure with buffers
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ * @param type differentiates between function and variable (use enum types_of_tokens)
+ * @return 0 on success, INTERNAL_ERROR on error
+ */
+int declaration_test(Resources *resources, index_t index_to_string, index_t index_to_root_node, int type);
+
+/** Deallocs memory used to store funtion arguments
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node pointer to index_t variable storing index to dynamic structure buffer
+ */
+int free_memory(Resources *resources, index_t index_to_root_node);
 
 #endif
