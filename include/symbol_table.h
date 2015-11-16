1610d03d43a1a4663bda6a012f21f58527eb950a
tomassykora <sykora.tms@gmail.com>
2015-11-16 14:25:44 +0100

    Update symbol_table.h

--- include/symbol_table.h
+++ include/symbol_table.h
@@ -27,11 +27,11 @@ enum types_of_tokens{
 	FUNC 
 };
 
-union Values {
+typedef union Values {
 	int i;	//value of integer variable or number of arguments of function
 	float f;	//value of float variable
 	index_t index_to_dynamic_buff;	//index to buffer with value of string variable
-};
+}TValues;
 
 typedef struct func_args {
 	int data_type;
@@ -118,6 +118,65 @@ int declaration_test(Resources *resources, index_t index_to_string, index_t inde
  * @param index_to_root_node index_t variable storing index to dynamic structure buffer
  */
 void free_memory(Resources *resources, index_t index_to_root_node);
+
+/** Load value of variable stored in symbol table
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node index_t variable storing index to dynamic structure buffer
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param value pointer to TValues union to store value of variable
+ * @return 0 on succes, INTERNAL_ERROR on failure
+ */
+int load_variable_value(Resources *resources, index_t index_to_root_node, index_t index_to_string, TValues *value);
+
+/** Gives data type of variable or data type of function return value
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node index_t variable storing index to dynamic structure buffer
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param type_of_node int value to differentiate between function and variable, one of enum types_of_tokens
+ * @return returns int value, one of enum data_types
+ */
+int get_data_type(Resources *resources, index_t index_to_root_node, index_t index_to_string, int type_of_node);
+
+/** Gives value of float variable
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node index_t variable storing index to dynamic structure buffer
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @return returns float value stored in symbol table
+ */
+float load_float_value(Resources *resources, index_t index_to_root_node, index_t index_to_string);
+
+/** Gives value of integer variable
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node index_t variable storing index to dynamic structure buffer
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @return returns integer value stored in symbol table
+ */
+int load_integer_value(Resources *resources, index_t index_to_root_node, index_t index_to_string);
+
+/** Gives value of string variable
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node index_t variable storing index to dynamic structure buffer
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @return returns index to dynamic buffer where string value is stored
+ */
+index_t load_string_value(Resources *resources, index_t index_to_root_node, index_t index_to_string);
+
+/** Gives number of arguments of a function
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node index_t variable storing index to dynamic structure buffer
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @return returns number of function's arguments
+ */
+int load_num_of_args(Resources *resources, index_t index_to_root_node, index_t index_to_string);
+
+/** Gives number of arguments of a function
+ * @param resources pointer to structure with buffers
+ * @param index_to_root_node index_t variable storing index to dynamic structure buffer
+ * @param index_to_string index to dynamic buffer (string of identifier)
+ * @param arg_index integer index to array of function's arguments 
+ * @return returns data type of x-th argument (x is value in arg_index)
+ */
+int load_arg_data_type(Resources *resources, index_t index_to_root_node, index_t index_to_string, int arg_index);
 
 #endif
 
