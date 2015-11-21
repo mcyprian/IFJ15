f8cd2de41b0b3fab39a3a73c398533506f22a50b
MichalDurista <miskodurista@gmail.com>
2015-11-21 15:37:18 +0100

    Add tests for syntax_check

--- tests/syntax_check_test.sh
+++ tests/syntax_check_test.sh
@@ -5,3 +5,8 @@ set -e
 ../bin/interpreter ./test_data/syntax_check_data/test
 ../bin/interpreter ./test_data/syntax_check_data/test2
 ../bin/interpreter ./test_data/syntax_check_data/test3
+../bin/interpreter ./test_data/syntax_check_data/test4
+../bin/interpreter ./test_data/syntax_check_data/test5
+../bin/interpreter ./test_data/syntax_check_data/test6
+../bin/interpreter ./test_data/syntax_check_data/test7
+../bin/interpreter ./test_data/syntax_check_data/test8
