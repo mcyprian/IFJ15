aff386bdbc19ac2eb34a279854b742ea5ec80af2
Michal Cyprian <mcyprian@redhat.com>
2015-12-02 10:38:12 +0100

    Add return value assert to syntax_check_test

--- tests/syntax_check_test.sh
+++ tests/syntax_check_test.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-set -e
+assert_retval() {
+    echo "ASSERT $2 $3"
+    $2
+    iRet=$?
+    if [ $iRet -eq $3 ]; then
+        return 0
+    else
+        echo "TEST $1 returns $iRet, expected $3" 
+        exit 1
+    fi
+}
 
-../bin/interpreter ./test_data/syntax_check_data/test
-../bin/interpreter ./test_data/syntax_check_data/test2
-../bin/interpreter ./test_data/syntax_check_data/test3
-../bin/interpreter ./test_data/syntax_check_data/test4
-../bin/interpreter ./test_data/syntax_check_data/test5
-../bin/interpreter ./test_data/syntax_check_data/test6
-../bin/interpreter ./test_data/syntax_check_data/test7
-../bin/interpreter ./test_data/syntax_check_data/test8
-! ../bin/interpreter ./test_data/syntax_check_data/test9
-../bin/interpreter ./test_data/syntax_check_data/test10
-! ../bin/interpreter ./test_data/syntax_check_data/test11
-../bin/interpreter ./test_data/syntax_check_data/test12
-../bin/interpreter ./test_data/syntax_check_data/test13
-../bin/interpreter ./test_data/syntax_check_data/fcb.cc
+# test            0 1 2 3 4 5 6 7 8 9 10 11 12 13
+expected_retVals=(0 0 0 0 0 0 0 0 0 2 0  2  9  0)
+
+for ((num=1; num<=12; ++num)); do
+    assert_retval "$num" "../bin/interpreter ./test_data/syntax_check_data/test$num" ${expected_retVals[$num]}
+done
+exit 0
