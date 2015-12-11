9f3b1c89d1cac445545e9bb7f4beb55af640d5cb
Michal Cyprian <m.cyprian@gmail.com>
2015-12-11 13:50:25 +0100

    Update expected return codes

--- tests/syntax_check_test.sh
+++ tests/syntax_check_test.sh
@@ -15,7 +15,7 @@ assert_retval() {
 # test            0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
 expected_retVals=(0 0 0 0 0 0 0 0 0 2 0  2  0  0  5)
 
-fb_expected_retVals=(0 0 0 3 3 0 0 1 1 1 2 2 2 2 2 2 3 3 3 3 3 3 3 3 4 4 4 4 4 5 0 2 3 3 3 8 2 2 2 2 2 2 2 2 7 1 1 1 1 1 1 8 8 0 8 0 9 9 9 9 0 2 3 2 1 4 8)
+fb_expected_retVals=(0 0 0 3 3 0 0 1 1 1 2 2 2 2 2 2 3 3 3 3 3 3 3 3 4 4 4 4 4 5 0 10 3 3 3 8 2 2 2 2 2 2 2 2 7 1 1 1 1 1 1 8 8 0 8 0 9 9 9 9 0 10 3 2 1 4 8)
 
 #for ((num=1; num<=14; ++num)); do
 #    assert_retval "$num" "../bin/interpreter ./test_data/syntax_check_data/test$num" ${expected_retVals[$num]}
