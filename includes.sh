a074e2189f990ec48bbade53e030b4a827ffa4a4
Michal Cyprian <m.cyprian@gmail.com>
2015-12-12 14:23:17 +0100

    Add -p switch to includes to append project name to head of each file

--- includes.sh
+++ includes.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 for cfile in $(ls *.c *.h); do
-
+    if [ "$1" == '-p' ]; then
+        sed -i '1 a\ * project: Implementace interpretu imperativního jazyka IFJ15' $cfile
+    fi
     for header in $(ls *.h); do
         sed -i "s/<$header>/\"$header\"/g" $cfile
     done
