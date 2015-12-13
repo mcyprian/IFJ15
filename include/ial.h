93fcb105d43ef639f48b0a2e7e2c3f19c703972f
MichalDurista <miskodurista@gmail.com>
2015-12-13 17:55:43 +0100

    Update ial.h

--- include/ial.h
+++ include/ial.h
@@ -25,11 +25,23 @@ int find(TDynamic_buffer *b, index_t string, index_t substring);
  * @param b pointer to TDyanmic buffer where string s is 
  * @param string index in the buffer where the string is
  * @return index to string where sorted string is
-*/
+ */
 index_t sort(TDynamic_buffer *b, index_t string);
 
+/** Algorithm for reconstructing the heap
+ * @param a pointer to the given string
+ * @param left left element
+ * @param right right element
+ */
 void SiftDown(char *a, int left, int right);
 
+/** Sorts characters in the given string using HeapSort algorithm
+ * @param a pointer to the given string
+ */
 void HeapSort(char *a);
 
+/** Computes the length of given string
+ * @param string pointer to the given string
+ * @return the length of given string
+ */
 int str_length(char *string);
